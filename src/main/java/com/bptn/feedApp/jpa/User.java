1513680e61f61f4e5553b8e07c00467721421982
Omar Ali <fullstackomar@gmail.com>
2023-03-09 09:06:39 -0500

    Add Instance variables and Annotations

--- src/main/java/com/bptn/feedApp/jpa/User.java
+++ src/main/java/com/bptn/feedApp/jpa/User.java
@@ -1,6 +1,7 @@
 package com.bptn.feedApp.jpa;
 
 import java.io.Serializable;
+
 import java.sql.Timestamp;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -12,6 +13,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 
 @Entity
@@ -45,6 +56,18 @@ public class User implements Serializable {
 		
 	@Column(name="\"createdOn\"")
 	private Timestamp createdOn;
+	
+	@JsonInclude(Include.NON_NULL)
+	@OneToOne(mappedBy="user", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	private Profile profile;
+
+	@JsonIgnore
+	@OneToMany(mappedBy="user")
+	private List<Feed> feeds;
+
+	@JsonIgnore
+	@OneToMany(mappedBy="user")
+	private List<FeedMetaData> feedMetaData;
 	
 	public User() {
 		
@@ -120,6 +143,30 @@ public class User implements Serializable {
 
 	public void setCreatedOn(Timestamp createdOn) {
 		this.createdOn = createdOn;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+	public List<Feed> getFeeds() {
+		return feeds;
+	}
+
+	public void setFeeds(List<Feed> feeds) {
+		this.feeds = feeds;
+	}
+
+	public List<FeedMetaData> getFeedMetaData() {
+		return feedMetaData;
+	}
+
+	public void setFeedMetaData(List<FeedMetaData> feedMetaData) {
+		this.feedMetaData = feedMetaData;
 	}
 
 	@Override
