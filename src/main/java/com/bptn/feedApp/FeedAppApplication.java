5e74dc406b3aff6c193956c7c1c3529b48303ff6
Omar Ali <fullstackomar@gmail.com>
2023-03-10 16:25:47 -0500

    Add EmailService class in service package, Addverify_email in templates

--- src/main/java/com/bptn/feedApp/FeedAppApplication.java
+++ src/main/java/com/bptn/feedApp/FeedAppApplication.java
@@ -3,9 +3,12 @@ package com.bptn.feedApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 //(exclude = {DataSourceAutoConfiguration.class })
+
+@EnableAsync
 @SpringBootApplication
 public class FeedAppApplication {
 
