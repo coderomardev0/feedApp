6b95df0b253f10cbb1a00475801bdda8b3130d8a
Omar Ali <fullstackomar@gmail.com>
2023-03-23 10:27:00 -0400

    add getUserFeeds method and mapping  in FeedController class

--- src/main/java/com/bptn/feedApp/controller/FeedController.java
+++ src/main/java/com/bptn/feedApp/controller/FeedController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.bptn.feedApp.jpa.Feed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import com.bptn.feedApp.domain.PageResponse;
 
 @CrossOrigin
 @RestController
@@ -38,6 +39,13 @@ public class FeedController {
 		return this.feedService.getFeedById(feedId);	
 	}
 	
+	@GetMapping("/user/{pageNum}/{pageSize}")
+	public PageResponse<Feed> getUserFeeds(@PathVariable int pageNum, @PathVariable int pageSize) {
+			
+		logger.debug("Getting User Feeds List, pageNum: {}, pageSize: {}", pageNum, pageSize);
+			
+		return this.feedService.getUserFeeds(pageNum, pageSize);	
+	}
 	
 	
 }
