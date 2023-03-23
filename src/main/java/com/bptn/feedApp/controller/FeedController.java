37874b48ae6129a01ad6e7404e43e0167952e1bd
Omar Ali <fullstackomar@gmail.com>
2023-03-23 09:04:50 -0400

    add FeedController class

---
+++ src/main/java/com/bptn/feedApp/controller/FeedController.java
@@ -0,0 +1,33 @@
+package com.bptn.feedApp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.bptn.feedApp.service.FeedService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.bptn.feedApp.jpa.Feed;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/feeds")
+public class FeedController {
+	@Autowired
+	FeedService feedService;
+
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@PostMapping
+	public Feed createFeed(@RequestBody Feed feed) {
+
+		logger.debug("Creating Feed");
+
+		return this.feedService.createFeed(feed);
+	}
+	
+	
+	
+}
