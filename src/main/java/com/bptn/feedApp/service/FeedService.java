5bf69aabf17592577d615687e42b73174070b399
Omar Ali <fullstackomar@gmail.com>
2023-03-23 10:23:19 -0400

    getUserFeeds page response method

--- src/main/java/com/bptn/feedApp/service/FeedService.java
+++ src/main/java/com/bptn/feedApp/service/FeedService.java
@@ -13,6 +13,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import com.bptn.feedApp.exception.domain.UserNotFoundException;
 import com.bptn.feedApp.jpa.Feed;
 import com.bptn.feedApp.jpa.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import com.bptn.feedApp.domain.PageResponse;
 
 
 @Service
@@ -42,6 +46,18 @@ public class FeedService {
 
 		return this.feedRepository.findById(feedId)
 		.orElseThrow(() -> new FeedNotFoundException(String.format("Feed doesn't exist, %d", feedId)));
+	}
+	
+	public PageResponse<Feed> getUserFeeds(int pageNum, int pageSize) {
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();		
+			
+		User user = this.userRepository.findByUsername(username)
+		             .orElseThrow(()-> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+					
+		Page<Feed> paged = this.feedRepository.findByUser(user, PageRequest.of(pageNum, pageSize, Sort.by("feedId").descending()));
+			
+		return new PageResponse<Feed>(paged);
 	}
 
 }
